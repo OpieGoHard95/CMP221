9d7b3e2536178166601067dc8a4f59a381e60e11
Jared Smith <jsmith2@saintjoe.edu>
2015-08-26 21:37:28 -0400

    Update HW1.java
    Fixed things that did not copy correctly from VI

--- HW1.java
+++ HW1.java
@@ -1,3 +1,26 @@
+Public class HW1{
+    public int a = 1;
+    public int b = 2;
+    public int c = 3;
+    public int result = 0;
+       if a != 13 {
+        result = a;}
+        else if b != 13 {
+          result = result + b;}
+        else if c != 13 {
+          result = result +c;}
+
+
+
+       System.out.println(result);
+
+        public int a = 1;
+    public int b = 2;
+    public int c = 13;
+    public int result = 0;
+       if a != 13 {
+        result = a;}
+
     public int c = 13;
     public int result = 0;
        if a != 13 {
@@ -19,6 +42,6 @@
           result = result + b;}
         else if c != 13 {
           result = result +c;}
-
+        System.out.println(result);
     }
                                                                41*1    Input 
