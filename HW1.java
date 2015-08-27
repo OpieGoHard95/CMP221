140f8681f63c7f20e41f92c171c23f972c24d87a
Jared Smith <jsmith2@saintjoe.edu>
2015-08-26 21:34:46 -0400

    Create HW1.java
    My first attempt at the problem

---
+++ HW1.java
@@ -0,0 +1,24 @@
+    public int c = 13;
+    public int result = 0;
+       if a != 13 {
+        result = a;}
+        else if b != 13 {
+          result = result + b;}
+        else if c != 13 {
+          result = result +c;}
+
+        System.out.println(result);
+
+        public int a = 1;
+    public int b = 13;
+    public int c = 1;
+    public int result = 0;
+       if a != 13 {
+        result = a;}
+        else if b != 13 {
+          result = result + b;}
+        else if c != 13 {
+          result = result +c;}
+
+    }
+                                                               41*1    Input 
