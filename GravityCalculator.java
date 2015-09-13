c3a009526f6dd0c782602f868ace6f7f83fc4236
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-09-12 22:18:40 -0400

    Finished Part One

---
+++ GravityCalculator.java
@@ -0,0 +1,12 @@
+class GravityCalculator {
+	public static void main(String[] args) {
+		double gravity = -9.81;
+		double intialVelocity = 0.0;
+		double fallingTime = 10.0;
+		double intialPosition = 0.0;
+		double finalPosition = 0.0;
+		System.out.println("The object's position after " + fallingTime + " seconds is " + finalPosition + "m.");
+	}
+}
+
+// It tells me that after 10 second the objects position is 0.0 meters.
